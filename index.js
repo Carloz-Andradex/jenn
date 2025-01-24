96a1a91302d04a8eef0649cdfdc65d6d458dc486
Carloz-Andradex <andradecarlos2503@hotmail.com>
2025-01-23 19:15:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-var bienvenida = "Escribe tu hermoso nombre 💖✨: ";
-var data = prompt(bienvenida);
-
-//Imprimir el nombre de la persona y transformalo a mayúsculas
-document.getElementById("data").innerHTML = data.toUpperCase();
+// Función para transformar el nombre a mayúsculas y mostrarlo en el elemento con ID "data"
+function mostrarNombre(nombre) {
+    document.getElementById("data").innerHTML = nombre.toUpperCase();
+}
 
 const yesBtn = document.querySelector('#yesBtn');
 
-//ESCRIBIR LO QUE PASARA EN CASO DE QUE SÍ
-yesBtn.addEventListener('click', function(){
-    alert('Ten por bonita'); //Puedes mandar los mensajes que quieras
-    location.href = 'https://www.youtube.com/watch?v=aSjflT_J0Xo'; //Cambiar el URL en YouTube de la canción que quieras 
+// Comportamiento del botón "Sí"
+yesBtn.addEventListener('click', function () {
+    alert('Ten por bonita'); // Puedes mandar los mensajes que quieras
+    location.href = 'https://www.youtube.com/watch?v=aSjflT_J0Xo'; // Cambiar el URL en YouTube de la canción que quieras
 });
 
 const noBtn = document.querySelector('#noBtn');
 
-//NO MOVER NADA AQUÍ
-noBtn.addEventListener('mouseover', function(){
-    const randomX = parseInt(Math.random()*100);
-    const randomY = parseInt(Math.random()*100);
+// Comportamiento del botón "No"
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
 
-    noBtn.style.setProperty('top', randomY+'%');
-    noBtn.style.setProperty('left', randomX+'%');
-    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+    noBtn.style.setProperty('top', randomY + '%');
+    noBtn.style.setProperty('left', randomX + '%');
+    noBtn.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
 });
-
